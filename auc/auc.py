ed3ef551fe64b0c92298ee84be063c269bd62aba
Ozan Müjde <46843218+ozanmujde@users.noreply.github.com>
2026-03-03 13:33:01 +0300

    Update solution for auc

--- auc/auc.py
+++ auc/auc.py
@@ -4,6 +4,4 @@ def auc(fpr, tpr):
     """
     Compute AUC (Area Under ROC Curve) using trapezoidal rule.
     """
-    # in here 
-    # return np.trapezoid(fpr,tpr,dx=fpr) this fails due to order I guess
     return np.trapezoid(tpr,fpr)
